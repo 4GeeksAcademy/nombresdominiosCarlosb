15aae242a6156377242d142b9ce2b8e45d2f09a1
carlossb21 <carlosandresb21@gmail.com>
2025-03-27 02:02:24 +0000

    cambios espanol

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
     
-    let pronoun = ['the', 'our'];
-    let adj = ['great', 'big'];
-    let noun = ['jogger', 'racoon'];
+    let pronoun = ['el', 'nuestro'];
+    let adj = ['gran', 'amigable'];
+    let noun = ['gato', 'perro'];
   
     for(let i=0;i<pronoun.length;i++){
       for(let j=0;j<adj.length;j++){
